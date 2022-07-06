f71ccea89fd3a01b07b1ed4edc29bd725ea6aa97
Dominik Schrempf <dominik.schrempf@gmail.com>
2022-07-06 12:21:34 +0200

    fix iaito

--- pkgs/hacking/iaito/default.nix
+++ pkgs/hacking/iaito/default.nix
@@ -8,15 +8,24 @@
 , zip
 }:
 
+# NOTE: The translations are installed to "$out/home/dominik/...", and are not
+# found by the executable.
+
+# NOTE: Other shared files such as the desktop file are installed to
+# "$out/usr/local/share/...", and are not found by the executable.
+
+# NOTE: Iaito reports a mismatch of the radare2 build and runtime versions. The
+# statement is wrong. The version string comparison fails somehow.
+
 stdenv.mkDerivation rec {
   pname = "iaito";
-  version = "5.7.0";
+  version = "5.7.0-dev";
 
   src = fetchFromGitHub {
     owner = "radareorg";
     repo = pname;
-    rev = version;
-    hash = "sha256-qEJTsS669eEwo2iiuybN72O5oopCaGEkju8+ekjw2zk=";
+    rev = "721886dffdcd644c31ce1558005ad5a1e0626452";
+    hash = "sha256-thVkpaRb/EV4azD3svN9+1/ZXIHPgS6QMKTqVZezHcQ=";
     fetchSubmodules = true;
   };
 
@@ -30,4 +39,16 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ pkg-config qttools wrapQtAppsHook zip ];
   buildInputs = [ radare2 ];
   # propagatedBuildInputs = [ ];
+
+  # Weird installation paths (/usr/...) crash the installer if DESTDIR is unset.
+  makeFlags = [ "DESTDIR=$(out)" ];
+
+  # Handle weird installation paths.
+  preFixup = ''
+    rm -r $out/home
+    mv $out/usr/local/bin $out/
+    mv $out/usr/local/share $out/
+    rmdir $out/usr/local
+    rmdir $out/usr
+  '';
 }
